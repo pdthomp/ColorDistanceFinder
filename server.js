91cd404df603ca20b8afc19e72c7bc7d23804ca2
Philip Thompson <philip.thompson.dev@gmail.com>
2016-06-05 18:34:43 -0400

    Update

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 var http = require('http');
+var url = require('url');
+var util = require('util');
+
 var hostname = '127.0.0.1';
 var port = 3000;
+var distance = 0;
 
 
 var server = http.createServer((req, res) => {
 	res.statusCode = 200;
 	res.setHeader('Content-Type', 'text/plain');
-	res.end('hello');
-
-	// url = require('url');
-	// var url_parts = url.parse(request.url, true);
-	// var query = url_parts.query;
-
+	
+	var url_parts = url.parse(req.url, true);
+	
+	distance = 	Math.pow((url_parts.query.r1 - url_parts.query.r2), 2) +
+				Math.pow((url_parts.query.r1 - url_parts.query.r2), 2) +
+				Math.pow((url_parts.query.r1 - url_parts.query.r2), 2);
+	
+	// res.end(util.inspect(distance, {showHidden: false, depth: null}));
+	
+	console.log(distance);
 });
 
 server.listen(port, hostname, () => {
